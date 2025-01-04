74b0efca307863f990576fc2ce982920335f29dc
Abhilash Srivastava <abhilash@cuberoot.tech>
2025-01-04 22:13:47 +0530

    fix: change message

--- dev/scripts/install_air.sh
+++ dev/scripts/install_air.sh
@@ -2,24 +2,24 @@
 
 # Check if 'air' is already installed
 if command -v air >/dev/null 2>&1; then
-  echo "✅ 'air' command is already installed."
+  echo "✅ air is already installed."
   exit 0
 fi
 
 # Check if 'go' is installed
 if ! command -v go >/dev/null 2>&1; then
-  echo "❌ Go is not installed. Please install Go first."
+  echo "❌ go is not installed. Please install go first."
   exit 1
 fi
 
 # Install 'air' using Go
-echo "🚀 Installing 'air' using Go..."
+echo "🚀 Installing air using go..."
 go install "github.com/cosmtrek/air@latest"
 
 # Verify installation
 if command -v air >/dev/null 2>&1; then
-  echo "✅ 'air' has been successfully installed."
+  echo "✅ air has been successfully installed."
 else
-  echo "❌ Failed to install 'air'. Please check for errors."
+  echo "❌ Failed to install air. Please check for errors."
   exit 1
 fi
